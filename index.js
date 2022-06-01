1d02f0474b4c53f38e15278a15dc8975517295d0
Akrem BOUZID <bouzidakrem7@gmail.com>
2022-06-01 03:37:40 +0100

    second commit

--- index.js
+++ index.js
@@ -17,5 +17,48 @@ const countDown = () => {
   document.querySelector(".hour").innerText = timeHour;
   document.querySelector(".minute").innerText = timeMinute;
   document.querySelector(".second").innerText = timeSecond;
+  /* Change the page*/
+  if (gap <= 0) {
+    document.querySelector(".day").innerText = 0;
+    document.querySelector(".hour").innerText = 0;
+    document.querySelector(".minute").innerText = 0;
+    document.querySelector(".second").innerText = 0;
+    const container = document.getElementById("container");
+    container.style.opacity = "0";
+    const newContainer = document.getElementById("newContainer");
+    newContainer.style.opacity = "1";
+  }
 };
+
 setInterval(countDown, 1000);
+
+/*
+const element = document.getElementById("btn-gift");
+console.log(element);
+const element1 = document.getElementById("btn-no-gift");
+console.log(element1);
+element.addEventListener("click",show);
+element1.addEventListener("click",hide);
+*/
+const showImage = document.getElementById("cake");
+const error = document.querySelector("#error");
+const hideAll = document.querySelector(".new-container");
+console.log(hideAll);  
+function show() {
+  showImage.style.opacity = "1";
+  document.getElementById("btn-no-gift").style.cursor = "pointer";
+  error.style.visibility = "hidden";
+}
+function hide() {
+  showImage.style.opacity = "0";
+  error.style.visibility = "visible";
+}
+let changeText = document.querySelector("#heroHeading");
+changeText.addEventListener("mouseenter",function(event){
+  event.target.innerText = "Happy Birthday Beautiful";
+})
+changeText.addEventListener("mouseout",function(event){
+  event.target.innerText = "Happy Birthday Vampire";
+})
+
+
